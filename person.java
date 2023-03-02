96217566f245ca335cd6e18382570066ac4ecab8
Rumiya-dev <56404235+Rumiya-dev@users.noreply.github.com>
2023-03-02 16:38:17 +0100

    Update person.java

--- person.java
+++ person.java
@@ -1,5 +1,39 @@
 package model;
 
 public class person {
+    private long id; // uniq ID
+    private String name; // person name
+    private int age; // person age
 
+
+    public person(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
